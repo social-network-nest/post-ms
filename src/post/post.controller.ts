608400ed23f997f2941ccfa6b4fb83d2ee3c2c8a
YTheSumsito <marcelo.jara@gmail.com>
2025-04-24 20:09:51 -0400

    create new post with user

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,13 +1,11 @@
 import { Controller } from '@nestjs/common';
 import { PostService } from './post.service';
 import { MessagePattern, Payload } from '@nestjs/microservices';
-import { UserService } from 'src/user/user.service';
 
 @Controller()
 export class PostController {
   constructor(
     private readonly postService: PostService,
-    private readonly userService: UserService
   ) {}
 
   @MessagePattern({ cmd: 'create' })
--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { PostService } from './post.service';
 import { PostController } from './post.controller';
-import { UserModule } from 'src/user/user.module';
 
 @Module({
-  imports: [UserModule],
   controllers: [PostController],
   providers: [PostService],
 })
